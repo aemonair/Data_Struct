b3a26c8eefec48f65ba1ccf8172ffa609c6c5c01
aemonair <v.aemonair@gmail.com>
2016-09-18 20:41:59 +0800

    head_file

---
+++ 04_iter_double_list/iterator.h
@@ -0,0 +1,30 @@
+#ifndef _ITERATOR_H_
+#define _ITERATOR_H_
+
+typedef struct Iterator
+{
+    void   *ptr;
+    int   index;
+    int    size;
+}Iterator;
+
+/*
+ *正向迭代器
+ * container(list、array、stack)容器
+ */
+
+#define FOREACH(iter, container) \
+    for(container->iter_head(&(iter), container); \
+    (iter).ptr; \
+    container->iter_next(&(iter), container))
+
+#define foreach FOREACH
+
+#define FOREACH_REVERSE(iter, container) \
+    for(container->iter_tail(&(iter), container); \
+    (iter).ptr; \
+    container->iter_prev(&(iter), container))
+
+#define foreach_reverse FOREACH_REVERSE
+
+#endif
